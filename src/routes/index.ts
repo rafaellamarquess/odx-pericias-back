174678ba694b851c89129093508607e3ebadf8bd
Rafaella <fariassrafaella@gmail.com>
2025-04-16 15:37:47 -0300

    docs: swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import userRoutes from "./userRoutes";
 import evidenceRoutes from "./evidenceRoutes";
 import reportRoutes from "./reportRoutes";
 import caseRoutes from "./caseRoutes";
+import authRoutes from "./authRoutes";
 
 const router = Router();
 
 // Agrupamento das rotas
 router.use("/user", userRoutes);
+router.use("/auth", authRoutes);
 router.use("/evidence", evidenceRoutes);
 router.use("/report", reportRoutes);
 router.use("/cases", caseRoutes); 
