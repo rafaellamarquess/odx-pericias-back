b5f7e1e68648eac017681da96c7c5c597c9a3177
Rafaella <fariassrafaella@gmail.com>
2025-04-16 12:30:03 -0300

    feat: adicionando listagem com filtros para casos

--- src/routes/caseRoutes.ts
+++ src/routes/caseRoutes.ts
@@ -6,11 +6,9 @@ import { Perfil } from "../models/UserModel";
 const router = Router();
 
 router.post("/", checkPermissions([Perfil.ADMIN, Perfil.PERITO]), CaseController.createCase);
+router.get("/", checkPermissions([Perfil.ADMIN, Perfil.PERITO]), CaseController.listCases);
 router.put("/:caseId", checkPermissions([Perfil.ADMIN, Perfil.PERITO]), CaseController.updateCase);
 router.delete("/:caseId", checkPermissions([Perfil.ADMIN, Perfil.PERITO]), CaseController.deleteCase);
 router.get("/case-title", checkPermissions([Perfil.ADMIN, Perfil.PERITO]), CaseController.getCaseTittle);
-
-// router.get("/", checkPermissions([Perfil.ADMIN, Perfil.PERITO]), caseController.getAllCases);
-// router.delete("/:caseId", checkPermissions([Perfil.ADMIN, Perfil.PERITO]), caseController.deleteCase);
 
 export default router;
