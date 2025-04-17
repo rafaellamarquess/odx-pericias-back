7685043886268edecc9df7d606524acfa6bbff24
Rafaella <fariassrafaella@gmail.com>
2025-04-17 18:40:16 -0300

    feat: concluindo rotas de CRUD para casos, evidências e usuário

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, editUser, listUsers, createUser } from "../controllers/UserController";
+import { deleteUser, updateUser, listUsers, createUser } from "../controllers/UserController";
 import { authenticateToken } from "../middlewares/authMiddleware";
 import { checkPermissions } from "../middlewares/permissionsMiddleware";
 import { Perfil } from "../models/UserModel";
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.post("/", authenticateToken, checkPermissions([Perfil.ADMIN]), createUser);
 router.get("/",authenticateToken, checkPermissions([Perfil.ADMIN]), listUsers);
-router.put("/:userId", authenticateToken, checkPermissions([Perfil.ADMIN]), editUser);
+router.put("/:userId", authenticateToken, checkPermissions([Perfil.ADMIN]), updateUser);
 router.delete("/:userId", authenticateToken, checkPermissions([Perfil.ADMIN]), deleteUser);
 
 export default router;
