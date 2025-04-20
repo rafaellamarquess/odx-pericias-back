70b4ba4fb27d4865c9280fcd13e874ac32a25ce3
Rafaella <fariassrafaella@gmail.com>
2025-04-20 17:09:04 -0300

    fix: variaveis cloudinary

--- src/middlewares/uploadMiddleware.ts
+++ src/middlewares/uploadMiddleware.ts
@@ -1,6 +1,14 @@
+import dotenv from "dotenv";
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { v2 as cloudinary } from "cloudinary";
+dotenv.config(); 
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
 
 const cloudinaryStorage = new CloudinaryStorage({
   cloudinary,
