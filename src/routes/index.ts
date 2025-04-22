74728214630f973174299699621cc38ceed5861b
Rafaella <fariassrafaella@gmail.com>
2025-04-21 21:06:26 -0300

    docs: swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import evidenceRoutes from "./evidenceRoutes";
 import reportRoutes from "./reportRoutes";
 import caseRoutes from "./caseRoutes";
 import authRoutes from "./authRoutes";
+import dashboardRoutes from "./dashboardRoutes";
 
 const router = Router();
 
@@ -13,6 +14,6 @@ router.use("/auth", authRoutes);
 router.use("/evidence", evidenceRoutes);
 router.use("/report", reportRoutes);
 router.use("/cases", caseRoutes); 
-router.use("/dashboard", reportRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 export default router;
