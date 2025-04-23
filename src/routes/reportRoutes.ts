685962ae0341e9d4ce2675df37f682e4a6c840e5
Rafaella <fariassrafaella@gmail.com>
2025-04-23 14:49:28 -0300

    ajustando rota sign report

--- src/routes/reportRoutes.ts
+++ src/routes/reportRoutes.ts
@@ -7,7 +7,7 @@ import { authenticateToken } from "../middlewares/authMiddleware";
 const router = Router();
 
 router.post("/", authenticateToken, checkPermissions([Perfil.ADMIN, Perfil.PERITO]), ReportController.createReport);
-router.post("/sing/:reportId", authenticateToken, checkPermissions([Perfil.ADMIN, Perfil.PERITO]), ReportController.assinarDigitalmente);
+router.post("/sign/:reportId", authenticateToken, checkPermissions([Perfil.ADMIN, Perfil.PERITO]), ReportController.assinarDigitalmente);
 router.put("/update/:reportId", authenticateToken, checkPermissions([Perfil.ADMIN, Perfil.PERITO]), ReportController.updateReport);
 router.delete("/delete/:reportId", authenticateToken, checkPermissions([Perfil.ADMIN, Perfil.PERITO]), ReportController.deleteReport);
 router.get("/list", authenticateToken, checkPermissions([Perfil.ADMIN, Perfil.PERITO]), ReportController.listReports);
