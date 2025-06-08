85cc05488714c7c77bb067695904b57bb33c0304
Rafaella <fariassrafaella@gmail.com>
2025-06-08 17:12:01 -0300

    fix: buscar vitimas

--- src/models/LaudoModel.ts
+++ src/models/LaudoModel.ts
@@ -1,12 +1,9 @@
 import mongoose, { Document, Schema, Types } from "mongoose";
 import { IEvidence } from "./EvidenceModel";
 import { IUser } from "./UserModel";
-import { IVitima } from "./VitimaModel";
 
 interface ILaudo extends Document {
-  evidencias?: Array<Types.ObjectId> | IEvidence[];
-  caso?: Types.ObjectId;
-  vitima: Types.ObjectId | IVitima;
+  evidencias: Array<Types.ObjectId> | IEvidence[];
   perito: Types.ObjectId | IUser;
   dadosAntemortem: string;
   dadosPostmortem: string;
@@ -21,17 +18,9 @@ const LaudoSchema = new Schema<ILaudo>({
     {
       type: Schema.Types.ObjectId,
       ref: "Evidence",
+      required: true,
     },
   ],
-  caso: {
-    type: Schema.Types.ObjectId,
-    ref: "Caso",
-  },
-  vitima: {
-    type: Schema.Types.ObjectId,
-    ref: "Vitima",
-    required: true,
-  },
   perito: {
     type: Schema.Types.ObjectId,
     ref: "User",
